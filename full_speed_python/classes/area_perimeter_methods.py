78406d70f913b74d7046de9f79710448489a3446
Sajan Thiara <64514761+SThiara@users.noreply.github.com>
2021-05-30 15:59:23 -0700

    completed classes section of full speed python

---
+++ full_speed_python/classes/area_perimeter_methods.py
@@ -0,0 +1,27 @@
+class Rectangle:
+  def __init__(self, x1, y1, x2, y2):
+    if x1 < x2 and y1 > y2:
+      self.x1 = x1
+      self.y1 = y1
+      self.x2 = x2
+      self.y2 = y2
+    else:
+      print("Incorrect coordinates of the rectangle!")
+                
+  def width(self):
+    return self.x2 - self.x1
+      
+  def height(self):
+    return self.y1 - self.y2
+  
+  def area(self):
+    return self.width() * self.height()
+  
+  def perimeter(self):
+    return 2*self.width() + 2*self.height()
+
+rectangle = Rectangle(2, 7, 8, 4)
+print(rectangle.width())
+print(rectangle.height())
+print("Area: " + str(rectangle.area()))
+print("Perimeter: " + str(rectangle.perimeter()))
